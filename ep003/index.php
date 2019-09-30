017cdbdaf924626b263640e03e003fe22d799fe2
elbersongames <elbersongames@gmail.com>
2019-09-30 07:34:36 -0400

    ABSTRAÇÃO DE BANCO DE DADOS COM DATA LAYER | PHP TIPS #002

---
+++ ep003/index.php
@@ -0,0 +1,20 @@
+<?php
+
+use Source\Support\Email;
+
+require __DIR__ . "/vendor/autoload.php";
+
+$email = new Email();
+//$email->add(
+//    "Ola Mundo, esse é meu Segundo E-mail",
+//    "<h1>Estou apenas testando!</h1>Espero que tenha dado certo!",
+//    "Elberson Dev",
+//    "elbersongames@gmail.com"
+////)->send();
+//)->attach("files/w.png", "Luiza Meu BB")->send();
+
+if (!$email->error()) {
+    var_dump(true);
+} else {
+    echo $email->error()->getMessage();
+}
