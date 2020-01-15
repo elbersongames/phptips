d1a7624278d61d53c1738af71ee58eca5383a008
elbersongames <elbersongames@gmail.com>
2020-01-15 11:18:17 -0400

    UPDATE update home page elbersongames

--- ep006/source/Config.php
+++ ep006/source/Config.php
@@ -2,8 +2,8 @@
 session_start();
 
 define("FACEBOOK", [
-    "app_id" => "",
-    "app_secret" => "",
-    "app_redirect" => "",
+    "app_id" => "1370166363156543",
+    "app_secret" => "8f0ceb4a12e43711a2e8ed49c4b59f3b",
+    "app_redirect" => "https://www.localhost/phptips/ep006",
     "app_version" => "v4.0"
 ]);
